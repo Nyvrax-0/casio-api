b92dff6eeb8fd264c3b9b94ccad62957cd933b91
Ibrohim <shaxabidinovibrohim@gmail.com>
2026-02-27 01:11:31 +0600

    Initial project

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = (
+        ('gshock', 'G-Shock'),
+        ('edifice', 'Edifice'),
+        ('vintage', 'Vintage'),
+    )
+
+
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
