cfdea891692323dac396da182f4a58b04c5bcbec
Ibrohim <shaxabidinovibrohim@gmail.com>
2026-03-07 14:24:33 +0600

    update api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = list(Product.objects.values())
+    return JsonResponse(products, safe=False)
